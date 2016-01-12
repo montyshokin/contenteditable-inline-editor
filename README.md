93468cfe6823bb7372d03f19c57fc8db1c4579b8
montyshokin <mashashokin@gmail.com>
2016-01-12 09:03:47 +0530

    Added source files for tutorial.

---
+++ README.md
@@ -0,0 +1,7 @@
+##Tuts+ Tutorial: Create an Inline Text Editor With contentEditable Attribute
+
+###Instructor: Monty Shokeen
+
+In this tutorial , I'll explain how to create a basic inline editor using the contentEditable Attribute. This is first part of two related tutorials. 
+
+Link to tutorial: [Create an Inline Text Editor With contentEditable Attribute](http://webdesign.tutsplus.com/tutorials/create-an-inline-text-editor-with-contenteditable-attribute--cms-25655) 
---
+++ js/editor.js
@@ -0,0 +1,42 @@
+var editBtn = document.getElementById('editBtn');
+var editables = document.querySelectorAll('#title, #author, #content')
+
+editBtn.addEventListener('click', function(e) {
+  if (editables[0].isContentEditable) {
+    // Disable Editing
+    editables[0].contentEditable = 'false';
+    editables[1].contentEditable = 'false';
+    editables[2].contentEditable = 'false';
+    // Change Button Text and Color
+    editBtn.innerHTML = 'Enable Editing';
+    editBtn.style.backgroundColor = '#F96';
+    // Save the data in localStorage 
+    for (var i = 0; i < editables.length; i++) {
+      localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
+    }
+  } else {
+    editables[0].contentEditable = 'true';
+    editables[1].contentEditable = 'true';
+    editables[2].contentEditable = 'true';
+    editBtn.innerHTML = 'Save Changes';
+    editBtn.style.backgroundColor = '#6F9';
+  }
+});
+
+setInterval(function() {
+  for (var i = 0; i < editables.length; i++) {
+    localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
+  }
+}, 5000);
+
+if (typeof(Storage) !== "undefined") {
+  if (localStorage.getItem('title') !== null) {
+    editables[0].innerHTML = localStorage.getItem('title');
+  }
+  if (localStorage.getItem('author') !== null) {
+    editables[1].innerHTML = localStorage.getItem('author');
+  }
+  if (localStorage.getItem('content') !== null) {
+    editables[2].innerHTML = localStorage.getItem('content');
+  }
+}
