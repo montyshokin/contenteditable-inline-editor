15639ff19a0ab30256a0a2cf89f109ca4b8ddc45
montyshokin <mashashokin@gmail.com>
2016-01-15 11:38:19 +0530

    Made changes to code structure

--- js/editor.js
+++ js/editor.js
@@ -1,33 +1,5 @@
 var editBtn = document.getElementById('editBtn');
-var editables = document.querySelectorAll('#title, #author, #content')
-
-editBtn.addEventListener('click', function(e) {
-  if (editables[0].isContentEditable) {
-    // Disable Editing
-    editables[0].contentEditable = 'false';
-    editables[1].contentEditable = 'false';
-    editables[2].contentEditable = 'false';
-    // Change Button Text and Color
-    editBtn.innerHTML = 'Enable Editing';
-    editBtn.style.backgroundColor = '#F96';
-    // Save the data in localStorage 
-    for (var i = 0; i < editables.length; i++) {
-      localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
-    }
-  } else {
-    editables[0].contentEditable = 'true';
-    editables[1].contentEditable = 'true';
-    editables[2].contentEditable = 'true';
-    editBtn.innerHTML = 'Save Changes';
-    editBtn.style.backgroundColor = '#6F9';
-  }
-});
-
-setInterval(function() {
-  for (var i = 0; i < editables.length; i++) {
-    localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
-  }
-}, 5000);
+var editables = document.querySelectorAll('#title, #author, #content');
 
 if (typeof(Storage) !== "undefined") {
   if (localStorage.getItem('title') !== null) {
@@ -40,3 +12,31 @@ if (typeof(Storage) !== "undefined") {
     editables[2].innerHTML = localStorage.getItem('content');
   }
 }
+
+editBtn.addEventListener('click', function(e) {
+  if (!editables[0].isContentEditable) {
+    editables[0].contentEditable = 'true';
+    editables[1].contentEditable = 'true';
+    editables[2].contentEditable = 'true';
+    editBtn.innerHTML = 'Save Changes';
+    editBtn.style.backgroundColor = '#6F9';
+  } else {
+    // Disable Editing
+    editables[0].contentEditable = 'false';
+    editables[1].contentEditable = 'false';
+    editables[2].contentEditable = 'false';
+    // Change Button Text and Color
+    editBtn.innerHTML = 'Enable Editing';
+    editBtn.style.backgroundColor = '#F96';
+    // Save the data in localStorage 
+    for (var i = 0; i < editables.length; i++) {
+      localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
+    }
+  }
+});
+
+setInterval(function() {
+  for (var i = 0; i < editables.length; i++) {
+    localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
+  }
+}, 5000);
